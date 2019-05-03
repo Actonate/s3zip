4b5ad47df4f603318a981420a7722fed3ab15bb4
divya khanani <divya.khanani@actonate.com>
2019-05-03 19:18:41 +0530

    added provision to pass ACL

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const _ = require('lodash')
 const archiver = require('archiver')
 
-const zipFiles = ({keys, bucket, outputFile, s3}) => {
+const zipFiles = ({keys, bucket, outputFile, acl, s3}) => {
   if (!keys || !bucket || !outputFile || !s3) throw new Error('keys, bucket, outputFile and s3 are required')
   if (!_.isArray(keys) || !keys.length) throw new Error('keys must be an array with at least one item')
   const archive = archiver('zip', {zlib: { level: 9 }})
   archive.on('error', err => {throw err})
   _.each(keys, key => archive.append(s3.getObject({Bucket: bucket, Key: key}).createReadStream(), { name: _.last(key.split('/')) }))
   archive.finalize()
-  return s3.upload({Bucket: bucket, Key: outputFile, Body: archive}).promise()
+  return s3.upload({Bucket: bucket, Key: outputFile, Body: archive, ACL: acl }).promise()
 }
 
 module.exports.zipFiles = zipFiles
